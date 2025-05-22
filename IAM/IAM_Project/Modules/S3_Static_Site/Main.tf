271ff43251836ba951aa681aec9f7ed145e2f06f
KiranSubhagan <subhagankiran21@gmail.com>
2025-05-22 21:19:35 +0530

    Update Main.tf

--- IAM/IAM_Project/Modules/S3_Static_Site/Main.tf
+++ IAM/IAM_Project/Modules/S3_Static_Site/Main.tf
@@ -1,1 +1,21 @@
+resource "aws_s3_bucket" "static_site" {
+bucket = var.bucket_name
+website {
+index_document = "index.html"
+error_document = "error.html"
+}
+}
+
+######################################################
+resource aws_s3_bucket_policy "allow_public_read"{
+policy = jsonencode({
+Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.static_site.arn}/*"
+    }]
+  })
+
 
