99bae50e84b7c308db6101fb7610ab8ad3407571
KiranSubhagan <subhagankiran21@gmail.com>
2025-05-23 00:43:55 +0530

    Update Main.tf

--- IAM/IAM_Project/Main.tf
+++ IAM/IAM_Project/Main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred AWS region
+}
+
+
+
 module "S3_Static_Site" {
   source = "./Modules/S3_Static_Site"
 bucket_name = "S3_STATIC_SITE"
