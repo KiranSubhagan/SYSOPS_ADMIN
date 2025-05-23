889da6faa23f842971af6d17fe5f0c217fd1a68a
KiranSubhagan <subhagankiran21@gmail.com>
2025-05-23 11:18:00 +0530

    Update Main.tf

--- IAM/IAM_Project/Modules/S3_Static_Site/Main.tf
+++ IAM/IAM_Project/Modules/S3_Static_Site/Main.tf
@@ -9,28 +9,33 @@ bucket = var.bucket_name
 
 
 ######################################################
-resource aws_s3_bucket_policy "allow_public_read"{
-bucket = aws_s3_bucket.static_site.id
-policy = jsonencode({
-Version = "2012-10-17",
-    Statement = [{
-      Sid = "AllowPublic"
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = ["s3:GetObject"]
-      Resource  = "${aws_s3_bucket.static_site.arn}/*"
-    },
-{
-Sid = "AllowTerraform"
-Effect = "Allow",
-Principal = "arn:aws:iam::941377118076:user/Github-Terraform-user",
-Action = "s3:*",
-Resource = ["${aws_s3_bucket.static_site.arn}/*", "${aws_s3_bucket.static_site.arn}"]
-}
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.static_site.id
 
-]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowPublic"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      },
+      {
+        Sid       = "AllowTerraform"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::941377118076:user/Github-Terraform-user"
+        }
+        Action   = "s3:*"
+        Resource = [
+          "${aws_s3_bucket.static_site.arn}/*",
+          "${aws_s3_bucket.static_site.arn}"
+        ]
+      }
+    ]
   })
-
 }
 
 ############################################################
